61fcb2ff4542386b1905b19699397c92abdc94c2
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-08-09 22:20:20 +0100

    Step 2: add SQLDelight database tooling

--- nextgen/build.gradle.kts
+++ nextgen/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("org.jetbrains.compose") version "1.11.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
     id("com.android.library") version "8.9.2" apply false
+    id("app.cash.sqldelight") version "2.1.0" apply false
 }
