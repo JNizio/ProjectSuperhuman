d02b79121bfe3a756beba5a676b02646f1082d37
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-08-09 22:40:05 +0100

    Step 3: centralize Kotlin Android plugin version

--- nextgen/build.gradle.kts
+++ nextgen/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform") version "2.2.20" apply false
+    kotlin("android") version "2.2.20" apply false
     id("org.jetbrains.compose") version "1.11.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
     id("com.android.application") version "8.9.2" apply false
