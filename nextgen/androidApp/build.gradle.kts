07d41ff4b8e0d394d9c34a098313ae837ffd0833
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-08-10 16:56:33 +0100

    Bump Project Superhuman to 11.0.19 for new logo

--- nextgen/androidApp/build.gradle.kts
+++ nextgen/androidApp/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.projectsuperhuman.next"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11018
-        versionName = "11.0.18"
+        versionCode = 11019
+        versionName = "11.0.19"
     }
 
     compileOptions {
