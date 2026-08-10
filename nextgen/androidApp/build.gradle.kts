27e4b60702545486f6b2752d7bd167fcd9634752
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-08-10 13:57:33 +0100

    Bump fixed sleep hero build to 11.0.11

--- nextgen/androidApp/build.gradle.kts
+++ nextgen/androidApp/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.projectsuperhuman.next"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11010
-        versionName = "11.0.10"
+        versionCode = 11011
+        versionName = "11.0.11"
     }
 
     compileOptions {
