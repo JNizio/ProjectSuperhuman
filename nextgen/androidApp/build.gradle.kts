c5684f5fad18630362248083509920201f6b0c1a
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-08-11 16:11:56 +0100

    Bump native nutrition redesign to 11.2.10

--- nextgen/androidApp/build.gradle.kts
+++ nextgen/androidApp/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.projectsuperhuman.next"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11209
-        versionName = "11.2.9"
+        versionCode = 11210
+        versionName = "11.2.10"
     }
 
     compileOptions {
