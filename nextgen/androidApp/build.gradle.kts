67295b3c942f74397d9aec1ae71da4f7df416b44
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-08-11 17:01:40 +0100

    Bump version to 11.2.12

--- nextgen/androidApp/build.gradle.kts
+++ nextgen/androidApp/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.projectsuperhuman.next"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11211
-        versionName = "11.2.11"
+        versionCode = 11212
+        versionName = "11.2.12"
     }
 
     compileOptions {
