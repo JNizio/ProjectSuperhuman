abaa913e0e75911d12824f0b4b54cfedc5b891db
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-08-13 17:43:20 +0100

    Add Trudy adapter passthrough check

--- nextgen/shared/src/commonTest/kotlin/com/projectsuperhuman/next/trudy/TrudyChecks.kt
+++ nextgen/shared/src/commonTest/kotlin/com/projectsuperhuman/next/trudy/TrudyChecks.kt
@@ -1,3 +1,21 @@
 package com.projectsuperhuman.next.trudy
 
-class TrudyOutputTest
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TrudyOutputTest {
+    @Test
+    fun nonEmotionalRequestsPassThroughDecoratorUnchanged() = runTest {
+        var calls = 0
+        val delegate = object : TrudyModelClient {
+            override suspend fun complete(request: TrudyModelRequest): TrudyModelResult {
+                calls += 1
+                return TrudyModelResult(responseText = "delegate")
+            }
+        }
+        val result = TrudyEmotionalModelClient(delegate).complete(TrudyModelRequest(userRequest = "How was my sleep?"))
+        assertEquals("delegate", result.responseText)
+        assertEquals(1, calls)
+    }
+}
