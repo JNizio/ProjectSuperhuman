7ea54b866c44b0d2e4d5ee924f6e07a0b8a008db
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-09-21 16:35:31 +0100

    Fix JSONArray import for core food snapshots

--- nextgen/androidApp/src/main/java/com/projectsuperhuman/next/LargeLocalFoodDatabase.kt
+++ nextgen/androidApp/src/main/java/com/projectsuperhuman/next/LargeLocalFoodDatabase.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.InputStreamReader
