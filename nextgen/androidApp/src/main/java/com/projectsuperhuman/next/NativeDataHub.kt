da14f4e809cf136fde9df57a508baf104661072a
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-09-22 12:19:52 +0100

    Persist food taxonomy tags in diary entries

--- nextgen/androidApp/src/main/java/com/projectsuperhuman/next/NativeDataHub.kt
+++ nextgen/androidApp/src/main/java/com/projectsuperhuman/next/NativeDataHub.kt
@@ -676,6 +676,8 @@ internal object NativeDataHub {
             put("isPlantFood", evidenceFood.isPlantFood.toString())
             put("plantFoodKind", evidenceFood.plantFoodKind.name)
             put("plantDiversityKey", evidenceFood.plantDiversityKey)
+            put("foodTags", evidenceFood.foodTags.map { it.name }.sorted().joinToString(","))
+            put("foodTaxonomyVersion", evidenceFood.foodTaxonomyVersion.toString())
             put("nutritionIntegrityWarning", evidenceFood.nutritionIntegrityWarning.orEmpty())
             put("nutritionApproximate", evidenceFood.nutritionApproximate.toString())
             put("unitSystemVersion", "4")
