ab06efac62cfa279b54e5077839983333f53aa48
Jakub Nizio <79284160+JNizio@users.noreply.github.com>
2026-09-22 08:26:29 +0100

    Fix plant counter Path import

--- nextgen/androidApp/src/main/java/com/projectsuperhuman/next/NativeNutritionExperienceV2.kt
+++ nextgen/androidApp/src/main/java/com/projectsuperhuman/next/NativeNutritionExperienceV2.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
